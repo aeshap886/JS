897389244b5bc87b8274f65a99d3cf6cdc9c9a26
aeshap886 <161817131+aeshap886@users.noreply.github.com>
2026-03-10 16:35:51 +0000

    variables

--- .devcontainer/01_basics/variables.js
+++ .devcontainer/01_basics/variables.js
@@ -4,17 +4,45 @@ var accountPassword = "12345"
 accountCity = "jaipur"
 
 // accountId = 2 //not allowed
-accountEmail = "Aesh@gmial.com"
-accountPassword = "23234"
-accountCity = "rajkot"
-let accountState;
+// accountEmail = "Aesh@gmial.com"
+// accountPassword = "23234"
+// accountCity = "rajkot"
+// let accountState;
 
-console.log(accountId);
+// console.log(accountId);
 
 /*
 prefer not to use var
 because of issue in block scope and functional scope
 */
 
+// console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+let score = null
+
+console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn ="" 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true ; 0=>false
+//aesha => true
+
+let someNumber = 33
+
+let stringNum = String(someNumber)
+console.log(stringNum);
+console.log(typeof stringNum)
