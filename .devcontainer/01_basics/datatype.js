8aba38b4601fcc48b05d442036ad6bba6e79d2f2
aeshap886 <161817131+aeshap886@users.noreply.github.com>
2026-03-12 12:29:46 +0000

    datatype

--- .devcontainer/01_basics/datatype.js
+++ .devcontainer/01_basics/datatype.js
@@ -21,7 +21,77 @@ let isLoggedIn = false
 
 // object
 
-console.log(typeof null); //null is object 
+// console.log(typeof null); //null is object 
 
 
-console.log(typeof undefined); //undefined
+// console.log(typeof undefined); //undefined
+
+// primitive 
+
+// 7 types: String, Number,Boolean,null,undefined,symbol, BigInt
+
+//java script is a dynamic type 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedin = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 34567865432345673567n
+
+
+//Reference (non primitive)
+
+//Array, Objects,Functions
+
+const heros=["shaktiman", "doga","naagraj"];
+
+let myObj ={
+    name :"Aesha",
+    age: 23
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+// console.log(typeof null);
+
+// console.log(typeof bigNumber);
+// console.log(typeof undefined);
+// console.log(typeof Object);
+// console.log(typeof myFunction);
+// console.log(typeof anotherId);
+
+
+//***************************************
+
+//Stack (primitive), Heap(non-Primitive)
+
+let myYoutubename = "Aeshapatel1234com"
+
+let anothername = myYoutubename
+
+anothername = "aeshu1233"
+console.log(anothername);
+console.log(myYoutubename);
+
+let userOne = {
+    email:"user@gmail.com",
+    upi: "user@123"
+}
+
+let userTwo = userOne
+
+userTwo.email = "Aesha@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
