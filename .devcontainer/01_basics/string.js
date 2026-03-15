8f18c1dbb641f633eaf31f37fc4c24c9d1f666fd
aeshap886 <161817131+aeshap886@users.noreply.github.com>
2026-03-15 12:38:29 +0000

    string

---
+++ .devcontainer/01_basics/string.js
@@ -0,0 +1,42 @@
+const name = "aesha"
+const repoCount = 50
+
+//console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('aesha')
+
+console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+
+//console.log(gameName.indexOf('e'));
+
+const newString = gameName.substring(0,4)
+
+//console.log(newString);
+
+const anotherString = gameName.slice(-4,4)
+//console.log(anotherString);
+
+
+const newStringOne = "   aesha  "
+//console.log(newStringOne);
+//console.log(newStringOne.trim());
+
+const url = "https://aesha886.com/aesha%22patel"
+
+//console.log(url.replace('%22','_'))
+
+console.log(url.includes('aesha'))
+
+console.log(gameName.split('-'));
+
