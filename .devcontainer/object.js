b848da274c32685ed48e903bbc0ed639d601c48d
aeshap886 <161817131+aeshap886@users.noreply.github.com>
2026-06-05 17:18:15 +0000

    obj

---
+++ .devcontainer/object.js
@@ -0,0 +1,20 @@
+//singleton
+//Object.create
+
+//object literals
+
+const JsUser = {
+    name: "Aesha",
+    "full name": " aesha usadadiya ",
+    age: 23,
+    location: "surat",
+    email: "Aesha123@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays: ["monday", "saturday"]
+
+}
+
+console.log(JsUser.email);
+
+console.log(JsUser["email"]);
+console.log(JsUser["full name"]);
