e46ead94c1ad16a1c4999168df776f92008c33c2
aeshap886 <161817131+aeshap886@users.noreply.github.com>
2026-06-05 10:45:40 +0000

    end

--- .devcontainer/Array/Array2.js
+++ .devcontainer/Array/Array2.js
@@ -9,7 +9,25 @@ const dc_heros = ["superman","flash","batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+ const all_new_heros = [...marvel_heros, ...dc_heros]
 
-console.log(all_new_heros)
+// console.log(all_new_heros)
 
+// Converts nested arrays into a single-level array   
+
+ const another_arr = [1, 2, 3, [4, 5, 6], 7 ,[6, 7 ,[4, 5]]]
+// const real_another_arr = another_arr.flat(Infinity) // flatten nested arrays
+// console.log(real_another_arr); 
+
+ 
+console.log(Array.isArray("Aesha"))
+
+console.log(Array.from("Aeshu"))
+
+console.log(Array.from({name: "Aesha"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2 ,score3));
