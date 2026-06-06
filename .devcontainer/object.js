d9776702587ef5b485ba2a7d1bddcea3d6aa579e
aeshap886 <161817131+aeshap886@users.noreply.github.com>
2026-06-06 18:22:38 +0000

    edit

--- .devcontainer/object.js
+++ .devcontainer/object.js
@@ -3,9 +3,13 @@
 
 //object literals
 
+const mySym = Symbol("key1")
+
+
 const JsUser = {
     name: "Aesha",
     "full name": " aesha usadadiya ",
+    [mySym]: "mykey1",
     age: 23,
     location: "surat",
     email: "Aesha123@gmail.com",
@@ -14,7 +18,30 @@ const JsUser = {
 
 }
 
-console.log(JsUser.email);
+// console.log(JsUser.email);
 
-console.log(JsUser["email"]);
-console.log(JsUser["full name"]);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "aesha@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "asdfgh.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello Js user");
+    
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello Js user, ${this.name}`); //
+    
+}
+
+console.log(JsUser.greeting);
+console.log(JsUser.greetingTwo());
+
+
+
+
